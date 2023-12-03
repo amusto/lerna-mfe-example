255af224c3f82b425f5696f10f059eb1876ee139
Armando Musto <armando.musto@gmail.com>
2023-12-03 18:22:57 -0500

    initial files

---
+++ app1/src/App.js
@@ -0,0 +1,24 @@
+import React, {Suspense} from "react";
+const RemoteApp = React.lazy(() => import("app2/App"));
+const RemoteNavigationApp = React.lazy(() => import("app3/App"));
+
+const App = () => {
+  return (
+    <div>
+      <div style={{
+        margin:"10px",
+        padding:"10px",
+        textAlign:"center",
+        backgroundColor:"greenyellow"
+      }}>
+        <h1>App1</h1>
+      </div>
+      <Suspense fallback={"loading..."}>
+        <RemoteApp/>
+        <RemoteNavigationApp />
+      </Suspense>
+    </div>)
+}
+
+
+export default App;
